42f0ef4a41fa3f7aa64236b2765fc7237d8b395f
scottjstewart <scottstewart965@gmail.com>
2019-10-11 14:16:09 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Result from "./result";
+import Keypad from "./keypad";
+import "./App.css";
 
 function App() {
+  const [result, setResult] = useState("");
+
+  function calculate() {
+    try {
+      setResult((eval(result) || "") + "");
+    } catch (e) {
+      setResult("error");
+    }
+  }
+
+  function reset() {
+    setResult("");
+  }
+
+  function backspace() {
+    setResult(result.slice(0, -1));
+  }
+
+  function onClick(button) {
+    if (button === "=") {
+      calculate();
+    } else if (button === "C") {
+      reset();
+    } else if (button === "CE") {
+      backspace();
+    } else {
+      setResult(result + button);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div className="calculator-body">
+          <h1>IEL Calculator</h1>
+          <Result result={result} />
+          <Keypad onClick={onClick} />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/result.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function result(props) {
+  return (
+    <div className="result">
+      <p>{props.result}</p>
+    </div>
+  );
+}
